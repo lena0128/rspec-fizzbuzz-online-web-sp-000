294ec4b52497af5ed8c5b2a1ad1d16aac94807c4
Lena Hou <lenahou2013@gmail.com>
2021-01-23 20:23:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(n)
+  if n / 3 == true
+    puts "fizz"
+  elsif n / 5 == true
+    puts "buzz"
+  elsif condition
+    puts "fizzbuzz"
+  else
+    puts ""
+  end
+end
+
+    
