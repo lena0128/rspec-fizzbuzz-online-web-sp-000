7ce8969fdc50e9e876f0b5e1c39a80cd4c8c8a9c
Lena Hou <lenahou2013@gmail.com>
2021-01-23 20:26:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(n)
-  if n / 3 == true
-    puts "fizz"
-  elsif n / 5 == true
-    puts "buzz"
-  elsif condition
+def fizzbuzz(int)
+  if int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  elsif int % 15 == 0
     puts "fizzbuzz"
   else
     puts ""
   end
 end
-
-    
