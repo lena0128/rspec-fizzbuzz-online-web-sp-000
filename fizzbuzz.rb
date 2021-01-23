08870ee1da42e99c64c875ce405b4cd0fca39c5f
Lena Hou <lenahou2013@gmail.com>
2021-01-23 20:27:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif int % 5 == 0
-    puts "Buzz"
+    return "Buzz"
   elsif int % 15 == 0
-    puts "fizzbuzz"
+    return "fizzbuzz"
   else
-    puts ""
+    return nil
   end
 end
