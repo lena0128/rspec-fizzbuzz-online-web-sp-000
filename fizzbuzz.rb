0adefd767bb473cd807e0a8ea263396e30ae8bd8
Lena Hou <lenahou2013@gmail.com>
2021-01-23 20:28:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-    return "Fizz"
+    "Fizz"
   elsif int % 5 == 0
     return "Buzz"
   elsif int % 15 == 0
